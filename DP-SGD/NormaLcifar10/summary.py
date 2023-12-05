ae5ff68142c09b8df575c3c9e705f866e93eaafd
iquhsek <shuqike.research@gmail.com>
2023-12-05 11:12:37 -0500

    go

--- DP-SGD/NormaLcifar10/summary.py
+++ DP-SGD/NormaLcifar10/summary.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 os.makedirs('./img',exist_ok=True)
 plt.title(r'resnet-18, $\epsilon=0.76$')
 x = np.array([0, 5, 10, 15, 20]) / 20
-acc = np.array([28, 39, 40, 40, 37])
+acc = np.array([43, 45, 52, 50, 37])
+std = np.array([0.92, 1.08, 0.57, 0.68, 1.09])
 plt.xlabel(r'$\epsilon_{\mathrm{lp}}/\epsilon$' + ' Fraction of Total Privacy Budget Alotted to LP')
 plt.plot(x, acc)
+plt.fill_between(x, y1=acc-std, y2=acc+std, alpha=0.3)
 plt.savefig('img/resnet18eps0.76.png')
