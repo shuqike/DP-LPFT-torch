23e2542c03da07d2bff926cad46297c40ca05084
iquhsek <shuqike.research@gmail.com>
2023-12-07 03:47:20 -0500

    go

---
+++ DP-SGD/MOCOcifar10/device0.sh
@@ -0,0 +1,3 @@
+python main.py --device cuda:0 --seed 0 --epoch 30 --lp-epoch 0 --lplr 0 --ftlr 0.1
+python main.py --device cuda:0 --seed 0 --epoch 30 --lp-epoch 0 --lplr 0 --ftlr 0.05
+python main.py --device cuda:0 --seed 0 --epoch 30 --lp-epoch 0 --lplr 0 --ftlr 0.01
---
+++ DP-SGD/MOCOcifar10/device1.sh
@@ -0,0 +1,3 @@
+python main.py --device cuda:1 --seed 0 --epoch 30 --lp-epoch 0 --lplr 0 --ftlr 0.005
+python main.py --device cuda:1 --seed 0 --epoch 30 --lp-epoch 0 --lplr 0 --ftlr 0.001
+python main.py --device cuda:1 --seed 0 --epoch 30 --lp-epoch 0 --lplr 0 --ftlr 0.0005
